ae15fc17c4d6df51fde1f22080c6d328a2b1e477
gunkiddedy <gunkiddedy@gmail.com>
2021-03-03 18:28:35 +0700

    Update OutgoingProductDetail.vue

--- src/components/warehouse/OutgoingProductDetail.vue
+++ src/components/warehouse/OutgoingProductDetail.vue
@@ -20,7 +20,7 @@
                 <!-- summary_table -->
                 <div class="summary_table px-4 py-8 w-full overflow-auto rounded font-semibold text-center hover:shadow-md bg-white">
                     <div class="grid grid-cols-2 gap-8 text-gray-500">
-                        <div class="my-card transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:bg-white bg-gray-50 shadow px-2 rounded border py-4">
+                        <div class="my-card transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:shadow-lg bg-white shadow px-2 rounded border py-4">
                             <table class="table text-left w-full">
                                 <tr>
                                     <td class="">Kode Transaksi</td>
@@ -80,7 +80,7 @@
                                 </tr>
                             </table>
                         </div>
-                        <div class="my-card transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:bg-white bg-gray-50 shadow px-2 rounded border py-4">
+                        <div class="my-card transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:shadow-lg bg-white shadow px-2 rounded border py-4">
                             <table class="table border-collapse text-left w-full">
                                 <tr>
                                     <td class="">Kurir</td>
@@ -104,7 +104,7 @@
                         </div>
 
                         <!-- ALAMAT GUDANG -->
-                        <div class="my-card transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:bg-white bg-gray-50 shadow px-2 rounded border py-4">
+                        <div class="my-card transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:shadow-lg bg-white shadow px-2 rounded border py-4">
                             <table class="table border-collapse text-left w-full">
                                 <tr>
                                     <td colspan="2" class="font-bold text-gray-700">Alamat Gudang</td>
@@ -155,7 +155,7 @@
                         </div>
 
                         <!-- ALAMAT PENGIRIMAN -->
-                        <div class="my-card transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:bg-white bg-gray-50 shadow px-2 rounded border py-4">
+                        <div class="my-card transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:shadow-lg bg-white shadow px-2 rounded border py-4">
                             <table class="table border-collapse text-left w-full">
                                 <tr>
                                     <td colspan="2 " class="font-bold text-gray-700">Alamat Pengiriman</td>
@@ -207,7 +207,7 @@
 
                         <!-- PRODUCT -->
                         <div class="col-span-2 my-8">
-                            <div class="add-retur py-8">
+                            <div class="add-retur py-8 border-t">
                                 <div class="input flex items-center justify-start">
                                     <div class="flex items-center mr-2">
                                         <input
@@ -215,12 +215,12 @@
                                             type="text" 
                                             placeholder="Transaction Code">
                                         <button 
-                                            class="bg-gray-200 px-3 py-2 rounded ml-2">
-                                            <span class="">Add</span>
+                                            class="hover:bg-gray-400 bg-gray-300 px-3 py-2 rounded ml-2">
+                                            <span class="text-gray-600">Add</span>
                                         </button>
                                     </div>
                                     <div>
-                                        <button class="flex items-center justify-between bg-blue-400 px-2 py-2 rounded">
+                                        <button class="flex items-center justify-between hover:bg-blue-600 bg-blue-500 px-2 py-2 rounded">
                                             <svg class="w-4 mr-1 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                                             <span class="text-white">Scan QR</span>
                                         </button>
@@ -230,7 +230,7 @@
                             <div class="text-left px-2 py-2 bg-gray-100 shadow">
                                 <span class="text-xl text-gray-500 font-bold">Infomasi Produk</span>
                             </div>
-                            <div class="my-card transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:bg-white bg-gray-50 shadow px-2 rounded-b border py-4">
+                            <div class="my-card transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:bg-white bg-white shadow px-2 rounded-b border py-4">
                                 
                                 <table class="table border-collapse text-left w-full">
                                     <thead class="font-bold text-gray-700">
