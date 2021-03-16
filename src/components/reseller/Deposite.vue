94a06c4524efad1eec5afa3a76bfef44c5d8f775
DESKTOP-JBJ600Q\Windows X <gunkiddedy@gmail.com>
2021-03-16 17:43:00 +0700

    Update Deposite.vue

--- src/components/reseller/Deposite.vue
+++ src/components/reseller/Deposite.vue
@@ -7,7 +7,7 @@
                 </div>
 
                 <div class="index_statistic grid lg:grid-cols-4 md:grid-cols-3 gap-4 pb-4 sm:grid-cols-2 xs:grid-cols-1 px-4 py-8">
-                    <div class="kotak3 px-4 py-8 bg-white rounded shadow-lg cursor-pointer font-semibold text-center hover:shadow-md">
+                    <div class="kotak3 px-4 py-4 bg-white rounded shadow-lg cursor-pointer font-semibold text-center hover:shadow-md">
                         <div class="flex flex-col items-center justify-start">
                             <svg class="w-10 mr-1 text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
@@ -17,7 +17,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="kotak3 px-4 py-8 bg-white rounded shadow-lg cursor-pointer font-semibold text-center hover:shadow-md">
+                    <div class="kotak3 px-4 py-4 bg-white rounded shadow-lg cursor-pointer font-semibold text-center hover:shadow-md">
                         <div class="flex flex-col items-center justify-start">
                             <svg class="w-10 mr-1 text-purple-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
@@ -27,7 +27,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="kotak3 px-4 py-8 bg-white rounded shadow-lg font-semibold text-center hover:shadow-md">
+                    <div class="kotak3 px-4 py-4 bg-white rounded shadow-lg font-semibold text-center hover:shadow-md">
                         <div class="flex flex-col items-center justify-start">
                             <svg class="w-10 mr-1 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -40,7 +40,7 @@
                     </div>
                     <div
                         @click="getTransactionHistory" 
-                        class="kotak3 px-4 py-8 bg-white rounded shadow-lg cursor-pointer font-semibold text-center hover:shadow-md">
+                        class="kotak3 px-4 py-4 bg-white rounded shadow-lg cursor-pointer font-semibold text-center hover:shadow-md">
                         <div class="flex flex-col items-center justify-start">
                             <svg class="w-10 mr-1 text-green-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
@@ -144,13 +144,13 @@
         </div>
         <!-- MODAL DETAIL-->
         <div v-if="showModalDetail" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
-            <div class="relative w-2/5 max-w-lg pl-20">
+            <div class="relative w-2/3 pl-20">
                 <!--content-->
                 <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                     <!--header-->
                     <div class="flex items-start justify-between p-2 border-b border-solid border-gray-300 rounded-t">
                         <span class="text-xl font-semibold pt-2">
-                        Customer Detail
+                        Deposite Detail
                         </span>
                         <button class="p-1 ml-auto bg-transparent border-0 text-black float-right text-2xl leading-none font-semibold outline-none focus:outline-none" @click="showModalDetail = !showModalDetail">
                             <svg class="w-8 text-gray-300" viewBox="0 0 20 20" fill="currentColor">
@@ -160,23 +160,70 @@
                     </div>
                     <!--body-->
                     <div class="relative px-4 py-2 flex-auto">
-                        <table class="table-auto text-gray-500 w-full">
+                        <table class="table-fixed text-gray-500 w-full">
                             <tbody>
                             <tr >
-                                <td class="py-2 px-2">Name</td>
-                                <td class="text-gray-600 font-semibold">{{customerDetail.cuctomer_servicename}}</td>
+                                <td class="py-2 px-2">Reseller</td>
+                                <td class="text-gray-600 font-semibold">
+                                    {{depositeDetail.reseller.name}}
+                                </td>
+                                <td class="py-2 px-2">Bukti transfer</td>
                             </tr>
-                            <tr class="bg-gray-100">
-                                <td class="py-2 px-2">Whatsapp Number</td>
-                                <td class="text-gray-600 font-semibold">{{customerDetail.whatsapp_number}}</td>
+                            <tr class="">
+                                <td class="py-2 px-2">Bank tujuan transfer</td>
+                                <td class="text-gray-600 font-semibold">
+                                    {{depositeDetail.admin_bank.bank.name}}
+                                </td>
+                                <td rowspan="8" class="text-gray-600 font-semibold">
+                                    <img class="object-cover w-full" src="https://images.unsplash.com/photo-1550853024-fae8cd4be47f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=507&q=80" alt="">
+                                </td>
                             </tr>
                             <tr >
-                                <td class="py-2 px-2">Username</td>
-                                <td class="text-gray-600 font-semibold">{{customerDetail.username}}</td>
+                                <td class="py-2 px-2">Nama rekening</td>
+                                <td class="text-gray-600 font-semibold">
+                                    {{depositeDetail.admin_bank.account_name}}</td>
                             </tr>
-                            <tr class="bg-gray-100">
+                            <tr class="">
+                                <td class="py-2 px-2">Nomor rekening</td>
+                                <td class="text-gray-600 font-semibold">
+                                    {{depositeDetail.admin_bank.account_no}}
+                                </td>
+                            </tr>
+                            <tr >
+                                <td class="py-2 px-2">Jumlah transfer</td>
+                                <td class="text-gray-600 font-semibold">
+                                    {{depositeDetail.amount}}
+                                </td>
+                            </tr>
+                            <tr class="">
+                                <td class="py-2 px-2">Tanggal dan jam transfer</td>
+                                <td class="text-gray-600 font-semibold">
+                                    {{depositeDetail.transfer_at}}
+                                </td>
+                            </tr>
+                            <tr >
+                                <td class="py-2 px-2">Di transfer dari Bank</td>
+                                <td class="text-gray-600 font-semibold">
+                                    {{depositeDetail.from_bank.name}}
+                                </td>
+                            </tr>
+                            <tr class="">
+                                <td class="py-2 px-2">Nama rekening</td>
+                                <td class="text-gray-600 font-semibold">
+                                    {{depositeDetail.account_name}}
+                                </td>
+                            </tr>
+                            <tr >
+                                <td class="py-2 px-2">Nomor rekening</td>
+                                <td class="text-gray-600 font-semibold">
+                                    {{depositeDetail.account_no}}
+                                </td>
+                            </tr>
+                            <tr class="">
                                 <td class="py-2 px-2">Status</td>
-                                <td class="text-gray-600 font-semibold">{{customerDetail.status==1 ? 'Active' : 'Inactive'}}</td>
+                                <td class="text-gray-600 font-semibold">
+                                    {{depositeDetail.status}}
+                                </td>
                             </tr>
                             </tbody>
                         </table>
@@ -191,7 +238,7 @@
             </div>
         </div><!-- end MODAL DETAIL-->
         <!-- MODAL HISTORY-->
-        <div v-if="showModalHistory" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
+        <div v-if="showModalHistory" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-40 outline-none focus:outline-none justify-center items-center flex">
             <div class="relative w-2/3 pl-20">
                 <!--content-->
                 <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
@@ -237,7 +284,13 @@
                                     <td class="border-collapse">{{item.type}}</td>
                                     <td class="border-collapse">{{item.posting_date}}</td>
                                     <td class="border-collapse">{{item.amount}}</td>
-                                    <td class="border-collapse">{{item.reference_id}}</td>
+                                    <td class="border-collapse">
+                                        <span
+                                            class="cursor-pointer hover:text-blue-400" 
+                                            @click="detailData(item.reference_id)">
+                                            Deposite #{{item.reference_id}}
+                                        </span>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
@@ -253,8 +306,8 @@
                 </div>
             </div>
         </div><!-- end MODAL HISTORY-->
-        <div v-if="showModalDetail" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        <div v-if="showModalHistory" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        <div v-if="showModalDetail" class="opacity-25 fixed inset-0 z-30 bg-black"></div>
+        <div v-if="showModalHistory" class="opacity-25 fixed inset-0 z-30 bg-black"></div>
     </main>
 </template>
 
@@ -357,7 +410,7 @@ export default {
                 perPage: 5,
             },
             keyword: '',
-            customerDetail: '',
+            depositeDetail: '',
             customerHistory: '',
             userBalance: 0,
             historyType: 'all',
@@ -410,13 +463,13 @@ export default {
             });
         },
         detailData(param){
-            axios.get(`/api/customer_service/${param}`,{
+            axios.get(`/api/deposites/${param}`,{
                 headers: {
                     'Authorization': 'Bearer ' + this.userToken
                 }
             })
             .then((response) => {
-                this.customerDetail = response.data;
+                this.depositeDetail = response.data.data;
                 this.showModalDetail = !this.showModalDetail;
                 console.log(response);
             })
